031b38757e116acb6b4b55ab98759e2c6a7f98d4
LenaBraunschweig <lena.braunschweig@gmail.com>
2024-05-15 16:58:50 +0000

    added some physics data reading practice

--- Practice_Data_Analysis.py
+++ Practice_Data_Analysis.py
@@ -1,4 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = (pd.read_csv('Data.csv'))
-print(df.to_string())
+
+# general practice
+'''df = pd.read_csv('Data.csv')
+df.drop_duplicates(inplace = True)
+print(df)
+
+print(df.info())
+print(df.corr())'''
+
+
+
+# physics lab 1 reading
+physFrame = pd.read_csv('Phys221Lab01.csv')
+print(physFrame.to_string())

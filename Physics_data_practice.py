78c9af752de09065cbff1cad03d67420bde2b932
LenaBraunschweig <lena.braunschweig@gmail.com>
2024-05-16 19:56:56 +0000

    oof

--- Physics_data_practice.py
+++ Physics_data_practice.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
-# creating the dataframe
+# comment: creating the dataframe
 physFrame = pd.read_csv('Phys221Lab01.csv')
 print(physFrame.to_string())
 
 editedPhysFrame = pd.read_csv('Phys221Lab01.csv')
-# fills the empty columns of the Gravitational force column to be the mean gravitational force
+# comment: fills the empty columns of the Gravitational force column to be the mean gravitational force
 editedPhysFrame['Gravitational Force'].fillna(editedPhysFrame['Gravitational Force'].mean(), inplace = True)
 
-# fills the empty columns of the Coulomb's force column to be the median coulomb force
+# comment: fills the empty columns of the Coulomb's force column to be the median coulomb force
 editedPhysFrame["Coulomb's Force"].fillna(editedPhysFrame["Coulomb's Force"].median(), inplace = True)
 print(editedPhysFrame.to_string())
 
 print()
-# displays all the information for the first row
+# comment: displays all the information for the first row
 print(editedPhysFrame.loc[0])
 

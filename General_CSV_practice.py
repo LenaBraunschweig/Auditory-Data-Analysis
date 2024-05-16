760e9cc9895b03c4ee496739673c0a54196ffda2
LenaBraunschweig <lena.braunschweig@gmail.com>
2024-05-16 17:59:55 +0000

    meep

--- General_CSV_practice.py
+++ General_CSV_practice.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-# general practice
+# creates the dataframe
 df = pd.read_csv('Data.csv')
 df.drop_duplicates(inplace = True)
 print(df)
 
+# displays the info and correlations between the data
 print(df.info())
 print(df.corr())

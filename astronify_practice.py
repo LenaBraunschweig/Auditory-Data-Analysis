40cef0967f15db89413ea511a4600694a3c82e7b
LenaBraunschweig <lena.braunschweig@gmail.com>
2024-06-08 03:02:29 +0000

    kinda working astronify?

--- astronify_practice.py
+++ astronify_practice.py
@@ -3,18 +3,21 @@ from astropy.table import Table
 from astronify.series import SoniSeries
 import pandas as pd
 
-file_reader = pd.read_csv("Data.csv")
-x_axis = file_reader['Duration']
-y_axis = file_reader['Pulse']
+file_reader = pd.read_csv("PhysLab01.csv")
+x_name = 'R'
+y_name = 'Theta'
+
+x_axis = file_reader[x_name]
+y_axis = file_reader[y_name]
 
 rows = len(x_axis)
 columns = 2
 
-data_table = Table(names = ('Duration', 'Pulse'))
+data_table = Table(names = (x_name, y_name))
 for index in range(len(x_axis)):
   data_table.add_row(vals = (x_axis[index], y_axis[index]))
 
+player = SoniSeries(data = data_table, time_col = x_name, val_col = y_name)
 
-player = SoniSeries(data = data_table, time_col = 'Duration', val_col = 'Pulse')
 player.sonify()
 player.play()
